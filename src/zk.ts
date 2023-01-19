e5f22708ff5bee39853088d80fbc80c51914f8b9
theNvN <thenvnsahu@gmail.com>
2023-01-19 19:26:51 +0530

    fix: return result

--- src/zk.ts
+++ src/zk.ts
@@ -5,7 +5,7 @@ import { poseidon } from 'xcircomlib';
 
 const { BigNumber } = ethers;
 
-export const poseidonHash = (...inputs: BigNumberish[]) => {
+export const poseidonHash = (...inputs: BigNumberish[]): string => {
   const hexInputs = inputs.map((input) => BigNumber.from(input).toHexString());
-  BigNumber.from(poseidon([...hexInputs])).toHexString();
+  return BigNumber.from(poseidon([...hexInputs])).toHexString();
 };
