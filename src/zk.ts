b8d8c33919fa8fc1cbe1b06f87b91750b65b84ad
theNvN <thenvnsahu@gmail.com>
2023-01-22 20:33:39 +0530

    feat: add poseidon contract artifact

--- src/zk.ts
+++ src/zk.ts
@@ -1,11 +1,18 @@
-import { ethers } from 'ethers';
+import { BytesLike, ethers } from 'ethers';
 import { type BigNumberish } from 'ethers';
 //@ts-ignore
 import { poseidon } from 'xcircomlib';
+//@ts-ignore
+import genContract from 'xcircomlib/src/poseidon_gencontract.js';
 
 const { BigNumber } = ethers;
 
 export const poseidonHash = (...inputs: BigNumberish[]): string => {
   const hexInputs = inputs.map((input) => BigNumber.from(input).toHexString());
   return BigNumber.from(poseidon([...hexInputs])).toHexString();
+};
+
+export const poseidonArtifact = {
+  abi: genContract.generateABI(2) as any[],
+  bytecode: genContract.createCode(2) as BytesLike,
 };
